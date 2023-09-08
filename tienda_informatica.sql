48722ea03f8e5ab5a0258b6f3d424adb201aa056
esccod8 <esccod8@test.com>
2023-09-08 16:24:18 -0600

    tienda informartica y diagrama p-final terminado

--- tienda_informatica.sql
+++ tienda_informatica.sql
@@ -1,19 +1,45 @@
-DROP IF EXISTS tienda_informatica;
+DROP DATABASE IF EXISTS tienda_informatica;
 CREATE DATABASE tienda_informatica;
 USE tienda_informatica;
-
-CREATE TABLE fabricante(
-codigo_fabricante INT(10); PRIMARY KEY auto_increment,
-nombre VARCHAR(100); not null,
+CREATE TABLE  `tienda_informatica`.`fabricante` (
+  `codigo` INT NOT NULL,
+  `nombre` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`codigo`)
 );
 
-CREATE TABLE productos(
-codigo INT(10)PRIMARY KEY auto_increment,
-nombre VARCHAR(100) not null,
-precio DECIMAL(7,2) not null,
-cantidad NUMERIC(1) not null,
--- restriccion
-CONSTRAINT productos_codigo_fabricante_codigo_productos
-FOREIGN KEY productos(codigo_fabricante)
+
+CREATE TABLE `tienda_informatica`.`producto`(
+  `codigo` INT NOT NULL,
+  `nombre` VARCHAR(100) NOT NULL,
+  `precio` DOUBLE NOT NULL,
+  `fabricante_codigo` INT NOT NULL,
+  PRIMARY KEY (`codigo`, `fabricante_codigo`),
+  CONSTRAINT `fk_producto_fabricante`
+    FOREIGN KEY (`fabricante_codigo`)
+    REFERENCES `tienda_informatica`.`fabricante` (`codigo`)
 );
--- insertamos valores de la tabla fabricante
+-- insertamos valores en la tabla de fabricante
+INSERT INTO `tienda_informatica`.`fabricante` (`codigo`, `nombre`) VALUES (1, 'Asus');
+INSERT INTO `tienda_informatica`.`fabricante` (`codigo`, `nombre`) VALUES (2, 'Lenovo');
+INSERT INTO `tienda_informatica`.`fabricante` (`codigo`, `nombre`) VALUES (3, 'Hewlett-Packard');
+INSERT INTO `tienda_informatica`.`fabricante` (`codigo`, `nombre`) VALUES (4, 'Samsung');
+INSERT INTO `tienda_informatica`.`fabricante` (`codigo`, `nombre`) VALUES (5, 'Seagate');
+INSERT INTO `tienda_informatica`.`fabricante` (`codigo`, `nombre`) VALUES (6, 'Crucial');
+INSERT INTO `tienda_informatica`.`fabricante` (`codigo`, `nombre`) VALUES (7, 'Gigabyte');
+INSERT INTO `tienda_informatica`.`fabricante` (`codigo`, `nombre`) VALUES (8, 'Huawei');
+INSERT INTO `tienda_informatica`.`fabricante` (`codigo`, `nombre`) VALUES (9, 'Xiaomi');
+SELECT * FROM tienda_informatica.fabricante;
+
+-- insertamos valores en la tabla producto
+INSERT INTO `tienda_informatica`.`producto` (`codigo`, `nombre`, `precio`, `fabricante_codigo`) VALUES (1, 'Disco duro SATA3 1TB', 86.99, 5);
+INSERT INTO `tienda_informatica`.`producto` (`codigo`, `nombre`, `precio`, `fabricante_codigo`) VALUES (2, 'Memoria RAM DDR4 8GB', 120, 6);
+INSERT INTO `tienda_informatica`.`producto` (`codigo`, `nombre`, `precio`, `fabricante_codigo`) VALUES (3, 'Disco SSD 1TB', 150.99, 4);
+INSERT INTO `tienda_informatica`.`producto` (`codigo`, `nombre`, `precio`, `fabricante_codigo`) VALUES (4, 'GeForce GTX 1050 TI', 185, 7);
+INSERT INTO `tienda_informatica`.`producto` (`codigo`, `nombre`, `precio`, `fabricante_codigo`) VALUES (5, 'GeForce GTX 1080 Xtreme', 755, 6);
+INSERT INTO `tienda_informatica`.`producto` (`codigo`, `nombre`, `precio`, `fabricante_codigo`) VALUES (6, 'Monitor 24 LED Full HD', 202, 1);
+INSERT INTO `tienda_informatica`.`producto` (`codigo`, `nombre`, `precio`, `fabricante_codigo`) VALUES (7, 'Monitor 27 LED Full HD', 245.99, 1);
+INSERT INTO `tienda_informatica`.`producto` (`codigo`, `nombre`, `precio`, `fabricante_codigo`) VALUES (8, 'Portatil Yoga 520', 559, 2);
+INSERT INTO `tienda_informatica`.`producto` (`codigo`, `nombre`, `precio`, `fabricante_codigo`) VALUES (9, 'Portatil ideapad 320', 444, 2);
+INSERT INTO `tienda_informatica`.`producto` (`codigo`, `nombre`, `precio`, `fabricante_codigo`) VALUES (10, 'Impresora HP Deskjet 3720', 59.99, 3);
+INSERT INTO `tienda_informatica`.`producto` (`codigo`, `nombre`, `precio`, `fabricante_codigo`) VALUES (11, 'Impresora HP Laserjet Pro M26nw', 180, 3);
+SELECT * FROM tienda_informatica.producto;
